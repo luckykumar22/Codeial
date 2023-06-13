10c3cb3124121c415afb9bd246f8a8a1bd0fd1c2
luck22y <luckykumar2204@gmail.com>
2023-06-13 18:56:49 +0530

    feat:sign up feature added

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,51 @@
-module.exports.profile = function(req, res){
-    return res.render('user_profile', {
-        title: 'User Profile'
-    })
-}
+const User = require("../models/user");
+
+module.exports.profile = function (req, res) {
+  return res.render("user_profile", {
+    title: "User Profile",
+  });
+};
+
+module.exports.signUp = function (req, res) {
+  return res.render("user_sign_up", {
+    title: "Codeial | Sign Up",
+  });
+};
+
+module.exports.signIn = function (req, res) {
+  return res.render("user_sign_in", {
+    title: "Codeial | Sign In",
+  });
+};
+
+// sign up data
+module.exports.create = function (req, res) {
+  if (req.body.password != req.body.confirm_password) {
+    return res.redirect("back");
+  }
+
+  User.findOne({ email: req.body.email }, function (err, user) {
+    if (err) {
+      console.log("Error in finding a user");
+      return;
+    }
+
+    if (!user) {
+      User.create(req.body, function (err, user) {
+        if (err) {
+          console.log("Error in creating a user");
+          return;
+        }
+
+        return res.redirect("/users/sign-in");
+      });
+    } else {
+      return res.redirect("back");
+    }
+  });
+};
+
+// sign in user
+module.exports.createSession = function (req, res) {
+  //todo later
+};

5f6008593ef3d1b2622933595695142c1c96e201
luck22y <luckykumar2204@gmail.com>
2023-06-15 10:40:51 +0530

    mongoDB cluster added

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1/codeial_development");
+// mongoose.connect("mongodb://127.0.0.1/codeial_development");
+mongoose.connect("mongodb+srv://admin22:tabtab123@cluster0.9mlsgec.mongodb.net/codeial?retryWrites=true&w=majority");
 
 const db = mongoose.connection;
 
